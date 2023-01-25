000e3b0f0d7bbe4d56cdf554be7fb39eecff0f3a
belaymit <2belamit@gmail.com>
2023-01-25 18:39:22 +0300

    added nav-menu-items and other objects

---
+++ src/commons/navLink.js
@@ -0,0 +1,72 @@
+const navLink = [
+  {
+    id: 1,
+    link: '/',
+    name: 'Home',
+    icon: 'estate',
+  },
+  {
+    id: 2,
+    link: '/about',
+    name: 'About',
+    icon: 'user',
+  },
+  {
+    id: 3,
+    link: '/skills',
+    name: 'Skills',
+    icon: 'file-alt',
+  },
+  {
+    id: 4,
+    link: '/services',
+    name: 'Service',
+    icon: 'briefcase-alt',
+  },
+  {
+    id: 5,
+    link: '/portfolio',
+    name: 'Portfolio',
+    icon: 'scenery',
+  },
+  {
+    id: 6,
+    link: '/contacts',
+    name: 'Contacts',
+    icon: 'message',
+  },
+];
+const socialLink = [
+  {
+    id: 1,
+    link: 'https://github.com/belaymit',
+    target: '_blank',
+    icon: 'github-alt',
+  },
+  {
+    id: 2,
+    link: 'https://www.linkedin.com/in/belay-birhanu-144ba714b/',
+    target: '_blank',
+    icon: 'linkedin',
+  },
+  {
+    id: 3,
+    link: 'https://www.instagram.com/',
+    target: '_blank',
+    icon: 'instagram',
+  },
+  {
+    id: 4,
+    link: 'https://dribbble.com/',
+    target: '_blank',
+    icon: 'dribbble',
+  },
+  {
+    id: 5,
+    link: 'https://facebook.com/',
+    target: '_blank',
+    icon: 'facebook-f',
+  },
+];
+
+export { navLink, socialLink };
