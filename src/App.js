42b22f9f11430f2896c55b45d6148ead6216d71a
belaymit <2belamit@gmail.com>
2023-01-26 10:23:56 +0300

    added Skills component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Skills from './components/skills/Skills';
+import FloatingNav from './components/ui/FloatingNav';
 
 const App = () => (
   <>
@@ -9,7 +11,9 @@ const App = () => (
     <main className="main">
       <Home />
       <About />
+      <Skills />
     </main>
+    <FloatingNav />
   </>
 );
 
