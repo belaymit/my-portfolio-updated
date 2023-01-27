0cc995bebc3351d76bed5c4f084bae009aa2d19c
belaymit <2belamit@gmail.com>
2023-01-27 09:15:55 +0300

    added Qualification component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Qualification from './components/qualification/Qualification';
+import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 import FloatingNav from './components/ui/FloatingNav';
 
@@ -12,6 +14,8 @@ const App = () => (
       <Home />
       <About />
       <Skills />
+      <Services />
+      <Qualification />
     </main>
     <FloatingNav />
   </>
