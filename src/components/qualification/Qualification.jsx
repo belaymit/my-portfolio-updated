7ea6f43538b183fad6776f7b549a90fb7325ea22
belaymit <2belamit@gmail.com>
2023-01-27 09:13:46 +0300

    created Qualification component and added qualification

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import QualificationSection from '../ui/QualificationSection';
+import '../../styles/qualification.scss';
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+
+    <section className="qualification section">
+      <h2 className="section-title">Qualification</h2>
+      <span className="section-subtitle">My Personal Journey</span>
+      <div className="qualification-container container">
+        <div className="qualification-tabs">
+          <div
+            className={toggleState === 1 ? 'qualification-button qualification-active button--flex'
+              : 'qualification-button button--flex'}
+            onClick={() => toggleTab(1)}
+            role="presentation"
+          >
+            <i className="uil uil-graduation-cap qualification-icon" />
+            {' '}
+            Education
+          </div>
+          <div
+            className={toggleState === 2 ? 'qualification-button qualification-active button--flex'
+              : 'qualification-button button--flex'}
+            onClick={() => toggleTab(2)}
+            role="presentation"
+          >
+            <i className="uil uil-briefcase-alt qualification-icon" />
+            {' '}
+            Experience
+          </div>
+        </div>
+        <QualificationSection toggle={toggleState} />
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
